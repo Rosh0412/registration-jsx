d1263c0b895525730017440de23ffaacdb429734
Rosh0412 <88614681+Rosh0412@users.noreply.github.com>
2021-09-07 07:48:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import  CrudComponent from './crud-component';
+
+function App() {
+  
+
+  return (
+    <div className="App">
+      <CrudComponent/></div>
+  );
+}
+
+export default App;
